27fdb9a6dcb319129fa50a654cefe8e6088f148e
petergambrill <petergambrill@gmail.com>
2015-11-17 21:13:47 +0000

    final version of cachematrix.R for assignment 2 of R programming coursera course

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below allow us to create a matrix with the property
+## that its inverse can be computed once and then cached.
 
-## Write a short comment describing this function
+## makeCacheMatrix ###
+## This function creates a special "matrix" object that can cache its inverse.
+## It takes one argument, a matrix, sets object 'i' equal to NULL and also 
+## stores a list of 3 functions.
 
 makeCacheMatrix <- function(x = matrix()) {
 
   i <- NULL
+  
+  ##will return the argument entered into the makeCacheMatrix
   getmatrix <- function() x
+  
+  ##will set object 'i' equal to the argument entered
   setinverse <- function(inverse) i <<- inverse
+  
+  ##will return the object 'i'
   getinverse <- function() i
 
   
@@ -15,18 +24,33 @@ makeCacheMatrix <- function(x = matrix()) {
  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve ##
+## This function will output the inverse of a matrix stored using the
+## 'makeCacheMatrix' function. If the inverse has already been calculated  
+## it will just return the cached value from the makeCacheMatrix function, 
+## (object 'i').
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()
-  if(!is.null(i)) {
+  
+  ## set object 'z' equal to the object 'i' from the makeCacheMatrix
+  ## function. If the inverse hasn't already been calculated it will just 
+  ## be NULL.
+  z <- x$getinverse()
+  
+  ##if object 'z' is not NULL then return 'z'
+  if(!is.null(z)) {
     message("getting cached data")
-    return(i)
-  }
+    return(z)
+  } 
+  ## Otherwie set the object 'matrix' equal to the argument entered into
+  ## makeCacheMatrix function. Then set object 'z' equal to the inverse
+  ## of that argument. Finally set the object 'i in the makeCacheMatrix 
+  ## equal to the inverse just calculated ('z') and then return that inverse
+  else {
   matrix <- x$getmatrix()
-  i <- solve(matrix)
-  x$setinverse(i)
-  return(i)
+  z <- solve(matrix)
+  x$setinverse(z)
+  return(z)
+  }
+  
 }
